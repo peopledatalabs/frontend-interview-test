59208b73e4e529e412dc272b003869c5cb30e424
AustinSeftonPDL <104467252+AustinSeftonPDL@users.noreply.github.com>
2022-07-27 14:37:59 -0700

    Instructions and minor tweaks (#1)
    * changes file name to chat.js
    
    * imports find from lodash
    
    * updates POST api/user to be GET api/user/:id
    
    * adds README
    
    * readme change
    
    * updates README, restores testsocket.js

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -3,16 +3,17 @@ import { find } from 'lodash';
 import users from '../../../constants/users';
 
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    if (req.body.id) {
-      const user = find(users, (user) => user.userId === req.body.id);
+  if (req.method === 'GET') {
+    const { id } = req.query;
+    if (id) {
+      const user = find(users, (user) => user.userId === Number(id));
       if (user) {
         res.status(200).json(user);
       } else {
         res.status(404).send({ message: 'User not found' });
       }
     } else {
-      res.status(400).send({ message: 'Bad request. Must provide an id in your body.' });
+      res.status(400).send({ message: 'Bad request. Must provide id.' });
     }
   } else {
     res.status(405).send({ message: 'Method not allowed' });
